a9b782a55fbcb36c105446854bf388ff1b61b911
Ganesh.P.Nischay <ganeshnischayp@gmail.com>
2018-10-30 23:48:39 +0530

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+var router = express.Router();
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -10,9 +11,19 @@ app.use(express.static('public'));
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Redirect all traffic to index.html
-app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.get('/files', function (req, res) {
+  res.sendFile(`${__dirname}/public/index1.html`)
+})
+
+// app.use((req, res) => res.sendFile(`${__dirname}/FileBufferReader/index.html`));
+
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.info('listening on %d', port);
 });
+
+
+
